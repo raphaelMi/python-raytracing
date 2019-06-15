f6fc151c53c2ffcfe4e0f9b3ead0eb867a192757
raphaelMi <raphael.m024@gmail.com>
2019-06-15 04:58:18 +0200

    added more structuring

--- render.py
+++ render.py
@@ -1,9 +1,11 @@
-#This module is responsible for rendering a scene.
+# This module is responsible for rendering a scene.
 
 
 import numpy as np
 
+
 class Ray:
+    # ray has information about
     def ___init___(self, x0, y0, z0, xdir, ydir, zdir):
         self.x0 = x0
         self.y0 = y0
@@ -12,8 +14,38 @@ class Ray:
         self.ydir = ydir
         self.zdir = zdir
 
+    def intersect(self, scene):
+        # calculate the earliest intersection with a primitive of the scene
+        pass
+        # TODO
+        # return primitive,coordinate,length
 
-def ray_iterator():
+    def evaluate(self, scene, recursion_depth):
+        # evaluates chromaticity and brightness of given ray recursively for a set depth of recursion
+        prim,coord,length = self.intersect(scene)
+        # TODO
+        # depending on the texture of the primitive, calculate more rays and combine result
+        # light_reflection = self.mirror(prim.normal).evaluate(scene,recursion_depth+1)
+        # light_source = ray(coord,scene.light.coord).eval...
+        # light_diffuse = ...
+        # light_refraction...
+        # return np.quick_maffs(light...)
+
+
+def ray_iterator(self, camera):
     pass
-    #this function enables smart iteration through every pixel on the FOV
-    #yield ray...
+    # this function enables iteration through every pixel on the FOV
+    # yield ray,(x,y)...
+
+
+def render_scene(scene, config):
+    # iterates through all pixels in viewport, traces rays and draws to bitmap
+
+    for ray,pixel in ray_iterator(scene.get_camera):
+        (r, g, b) = ray.evaluate(ray, scene, 0)
+
+
+
+
+
+
--- scene.py
+++ scene.py
@@ -1,12 +1,15 @@
-#This module is responsible for structuring the geometry and texture of a scene in a manageable way
+# This module is responsible for structuring the geometry and texture of a scene in a manageable way
 
 import numpy as np
 
+
 class Primitive:
-    #stores a single triangle or sphere, along with texture and color data
+    # stores a single triangle or sphere, along with texture and color data
     pass
 
+
 class Camera:
+    # stores information about viewport and FOV
     def ___init___(self):
         pass
 
@@ -18,3 +21,17 @@ class Camera:
     def set_FOV(self,width_angle,height_angle):
         self.width_angle = width_angle
         self.height_angle = height_angle
+
+
+class Scene:
+    # container for all primitives and camera
+    def ___init___(self):
+        pass
+
+
+def read_scene_from_file(file):
+    # reads scene from .obj file
+
+    scene = 0
+
+    return scene
